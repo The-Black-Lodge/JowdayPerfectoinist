02a2ec74d5fd77d3d478eb5c89a3b16896568df2
Jen Guerra <southpawgeek@users.noreply.github.com>
2024-06-12 13:44:43 -0400

    add more options

--- src/config.lua
+++ src/config.lua
@@ -2,7 +2,11 @@
 ---@diagnostic disable
 
 return {
-  PerfectChance = 1
+  PerfectChance = 2,
+    MaxPerfectChance = 10,
+  AllowPerfectSacrifice = true
 }, {
-  PerfectChance = "% Chance of Perfect boon. Default: 1, Max: 5"
+  PerfectChance = "% Chance of Perfect boon. Default: 2",
+    MaxPerfectChance = "Max % allowed on slider. Default: 10",
+  AllowPerfectSacrifice = "Allows Heroic boons to upgrade to Perfect during Sacrifice. Default: true"
 }
--- src/imgui.lua
+++ src/imgui.lua
@@ -5,12 +5,19 @@ function public.drawPerfectPlugin()
     local mods = rom.mods
     local mod = mods['Jowday-BoonBuddy']
 
-        rom.ImGui.Text("Perfectoinist")
-        rom.ImGui.PushStyleColor(rom.ImGuiCol.Text, 0.62, 1, 1, 1)
-        value, selected = rom.ImGui.SliderInt("% Perfect", config.PerfectChance, 0, 5)
-        if selected then
-            config.PerfectChance = value
-            mod.adjustRarityValues()
-        end
-        rom.ImGui.PopStyleColor()
+    rom.ImGui.Text("Perfectoinist")
+    rom.ImGui.PushStyleColor(rom.ImGuiCol.Text, 0.62, 1, 1, 1)
+    value, selected = rom.ImGui.SliderInt("% Perfect", config.PerfectChance, 0, config.MaxPerfectChance)
+    if selected then
+        config.PerfectChance = value
+        mod.adjustRarityValues()
+    end
+    rom.ImGui.PopStyleColor()
+
+    value, checked = rom.ImGui.Checkbox("Allow Heroic boons to upgrade to Perfect (Sacrifice)",
+        config.AllowPerfectSacrifice)
+    if checked then
+        config.AllowPerfectSacrifice = value
+        mod.adjustRarityValues()
+    end
 end
